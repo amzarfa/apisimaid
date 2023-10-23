b04e95f00fc62556c8f963c5528fd4f031373269
amzarfa <amzarfadliatma7@gmail.com>
2023-10-23 12:06:55 +0700

    Update tabel Pegawai

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,5 +31,6 @@ class DatabaseSeeder extends Seeder
         $this->call(KodeJenisPengawasanSeeder::class);
         $this->call(KodeAreaPengawasanSeeder::class);
         $this->call(KodeTingkatResikoSeeder::class);
+        $this->call(TabelDataPegawaiSeeder::class);
     }
 }
